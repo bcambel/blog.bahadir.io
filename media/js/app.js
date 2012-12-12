d33c69bda3de03332d5ab471d8a2887fcd987a77
Bahadir Cambel <bcambel@gmail.com>
2012-12-12 15:41:54 +0100

    add markdown and media files

---
+++ media/js/app.js
@@ -0,0 +1,10 @@
+(function($){
+	$(init);
+	function init(){
+		var $content;
+		$.get("/posts/entrepreneurship-and-failure.md",function(data){
+			$content = markdown.toHTML(data);
+			$('#content_html').html($content);
+		});
+	}
+})(jQuery);
---
+++ posts/entrepreneurship-and-failure.md
@@ -0,0 +1,6 @@
+Entrepreneurship is hard!
+========================
+
+Why is it so hard ?
+
+Whatever dream or goal you have, you are chasing something. Something that does not belong to you, and never will be.
